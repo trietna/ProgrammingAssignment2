0405541976c2ea6910e095529c446ddc879c3906
trietna <nguyenanhtriet@gmail.com>
2017-03-30 19:48:38 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -9,7 +9,7 @@ makeCacheMatrix <- function(x = matrix()) {
     x<<-y
     inv<<-NULL
   }
-  get<-function() x
+  get<-function(y) x
   setinverse<-function(inverse) inv<<-inverse
   getinverse<-function() inv
   list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
